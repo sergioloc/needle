bd7f7e1e14b26bb4a9f6b93aa0d124160966dbfc
Sergio López <sergioloc2@gmail.com>
2020-10-23 00:28:58 +0200

    Navigate from profile to edit

--- app/src/main/java/com/slc/amarn/views/ProfileFragment.kt
+++ app/src/main/java/com/slc/amarn/views/ProfileFragment.kt
@@ -1,19 +1,29 @@
 package com.slc.amarn.views
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.slc.amarn.R
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initButtons()
+    }
+
+    private fun initButtons(){
+        fab_edit.setOnClickListener {
+            val intent = Intent(context, EditActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
