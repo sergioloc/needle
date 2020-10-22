d14a3ce41544d0ef61a821ac97dc189c24ec318e
Sergio López <sergioloc2@gmail.com>
2020-10-22 23:23:36 +0200

    New item user & swipe fragment

--- app/src/main/java/com/slc/amarn/adapters/CardStackAdapter.kt
+++ app/src/main/java/com/slc/amarn/adapters/CardStackAdapter.kt
@@ -3,10 +3,8 @@ package com.slc.amarn.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.slc.amarn.R
 import com.slc.amarn.models.User
 
@@ -16,7 +14,7 @@ class CardStackAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return ViewHolder(inflater.inflate(R.layout.item_spot, parent, false))
+        return ViewHolder(inflater.inflate(R.layout.item_user, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
--- app/src/main/java/com/slc/amarn/models/User.kt
+++ app/src/main/java/com/slc/amarn/models/User.kt
@@ -3,9 +3,10 @@ package com.slc.amarn.models
 data class User(
     val id: Int,
     val name: String,
-    val age: String,
+    val age: Int,
     val lbg: String,
     val membership: Int,
+    val gender: Int,
     val orientation: Int,
     val description: String
 )
