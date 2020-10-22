24bd97f4e5e6ea603c388a8cf7b2acc2d43de6d2
Sergio López <sergioloc2@gmail.com>
2020-10-22 20:47:14 +0200

    New card adapter

--- app/src/main/java/com/slc/amarn/adapters/CardStackAdapter.kt
+++ app/src/main/java/com/slc/amarn/adapters/CardStackAdapter.kt
@@ -1,4 +1,49 @@
 package com.slc.amarn.adapters
 
-class CardStackAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.slc.amarn.R
+import com.slc.amarn.models.User
+
+class CardStackAdapter(
+    private var users: List<User> = emptyList()
+) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(inflater.inflate(R.layout.item_spot, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = users[position]
+        holder.name.text = "${user.name}, ${user.age}"
+        holder.lbg.text = user.lbg
+        /*Glide.with(holder.image)
+            .load(spot.url)
+            .into(holder.image)*/
+    }
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+
+    fun setUsers(spots: List<User>) {
+        this.users = spots
+    }
+
+    fun getUsers(): List<User> {
+        return users
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.item_name)
+        var lbg: TextView = view.findViewById(R.id.item_lbg)
+        //var image: ImageView = view.findViewById(R.id.item_image)
+    }
+
 }
