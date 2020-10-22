b2c93775da3dade8e87aa111d47ad7bfbd63a6cb
Sergio López <sergioloc2@gmail.com>
2020-10-22 18:21:58 +0200

    Change icon color on selection

--- app/src/main/java/com/slc/amarn/MainActivity.kt
+++ app/src/main/java/com/slc/amarn/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.slc.amarn
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
 import com.slc.amarn.adapters.PagerAdapter
 import com.slc.amarn.views.MatchFragment
 import com.slc.amarn.views.ProfileFragment
 import com.slc.amarn.views.SearchFragment
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +33,24 @@ class MainActivity : AppCompatActivity() {
         tabs.getTabAt(0)?.setIcon(R.drawable.ic_profile)
         tabs.getTabAt(1)?.setIcon(R.drawable.ic_hot)
         tabs.getTabAt(2)?.setIcon(R.drawable.ic_message)
+
+        viewPager.currentItem = 1
+        val accentFilter: ColorFilter = PorterDuffColorFilter(resources.getColor(R.color.colorAccent), PorterDuff.Mode.SRC_IN)
+        val whiteFilter: ColorFilter = PorterDuffColorFilter(resources.getColor(R.color.white), PorterDuff.Mode.SRC_IN)
+        tabs.getTabAt(1)?.icon?.colorFilter = accentFilter
+
+        tabs.addOnTabSelectedListener(object :
+            TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                tab.icon?.colorFilter = accentFilter
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                tab.icon?.colorFilter = whiteFilter
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) { }
+
+        })
     }
 }
