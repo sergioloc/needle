2d04e096aaa9d7b25033f0d6b9aadf0f96be4c28
Sergio López <sergioloc2@gmail.com>
2020-10-23 17:00:08 +0200

    New photo number indicator

--- app/src/main/java/com/slc/amarn/views/UserActivity.kt
+++ app/src/main/java/com/slc/amarn/views/UserActivity.kt
@@ -1,10 +1,12 @@
 package com.slc.amarn.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.slc.amarn.R
 import com.slc.amarn.adapters.PhotoAdapter
 import kotlinx.android.synthetic.main.activity_user.*
+
 
 class UserActivity : AppCompatActivity() {
 
@@ -15,5 +17,16 @@ class UserActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user)
         var adapter = PhotoAdapter(applicationContext, images)
         vp_photos.adapter = adapter
+        chip_info.text = "1/${images.size}"
+
+        vp_photos.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) { }
+
+            override fun onPageSelected(position: Int) {
+                chip_info.text = "${position+1}/${images.size}"
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
     }
 }
