959284d74f0fbb2313631a00f212175b72d23ee6
Sergio López <sergioloc2@gmail.com>
2020-10-25 13:33:23 +0100

    Navigate from login to register

--- app/src/main/java/com/slc/amarn/views/LoginActivity.kt
+++ app/src/main/java/com/slc/amarn/views/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.slc.amarn.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.slc.amarn.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        initButtons()
+    }
+
+    private fun initButtons(){
+        tv_create.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
