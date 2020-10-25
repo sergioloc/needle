a3a3dc4ce0bb8cf70b02030dbaf343c6fae17aee
Sergio López <sergioloc2@gmail.com>
2020-10-25 21:40:54 +0100

    New google authentication

--- app/src/main/java/com/slc/amarn/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/slc/amarn/viewmodels/LoginViewModel.kt
@@ -1,22 +1,29 @@
 package com.slc.amarn.viewmodels
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginViewModel: ViewModel() {
 
     private val _state: MutableLiveData<Result<Boolean>> = MutableLiveData()
-    val state: LiveData<Result<Boolean>>
-        get() = _state
+    val state: LiveData<Result<Boolean>> get() = _state
+
+    private val _googleSignInClient: MutableLiveData<Result<GoogleSignInClient>> = MutableLiveData()
+    val googleSignInClient: LiveData<Result<GoogleSignInClient>> get() = _googleSignInClient
 
     init {
         if (FirebaseAuth.getInstance().currentUser != null)
             _state.postValue(Result.success(true))
     }
 
-    fun signIn(email: String, password: String){
+    fun signInWithMain(email: String, password: String){
         if (email.isNullOrEmpty() || password.isNullOrEmpty()){
             _state.postValue(Result.failure(Throwable("Complete all fields")))
         }
@@ -28,6 +35,27 @@ class LoginViewModel: ViewModel() {
                 else {
                     _state.postValue(Result.failure(Throwable(it.exception)))
                 }
+            }
+        }
+    }
+
+    fun getGoogleCredential(id: String, context: Context){
+        val googleConfig = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(id)
+            .requestEmail()
+            .build()
+        val googleClient = GoogleSignIn.getClient(context, googleConfig)
+        googleClient.signOut()
+        _googleSignInClient.postValue(Result.success(googleClient))
+    }
+
+    fun signInWithCredential(credential: AuthCredential){
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                _state.postValue(Result.success(true))
+            }
+            else {
+                _state.postValue(Result.failure(Throwable(it.exception)))
             }
         }
     }
--- app/src/main/java/com/slc/amarn/views/LoginActivity.kt
+++ app/src/main/java/com/slc/amarn/views/LoginActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
 import com.slc.amarn.MainActivity
 import com.slc.amarn.R
 import com.slc.amarn.viewmodels.LoginViewModel
@@ -16,6 +20,7 @@ import kotlinx.android.synthetic.main.activity_register.*
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var loginViewModel: LoginViewModel
+    private val GOOGLE_SIGN_IN = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +38,11 @@ class LoginActivity : AppCompatActivity() {
         }
 
         btn_login.setOnClickListener {
-            loginViewModel.signIn(et_mail.text.toString(), et_password.text.toString())
+            loginViewModel.signInWithMain(et_mail.text.toString(), et_password.text.toString())
+        }
+
+        btn_google.setOnClickListener {
+            loginViewModel.getGoogleCredential(getString(R.string.default_web_client_id), this)
         }
     }
 
@@ -50,5 +59,26 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         )
+
+        loginViewModel.googleSignInClient.observe(this,
+            Observer<Result<GoogleSignInClient>> {
+                it.onSuccess { result ->
+                    startActivityForResult(result.signInIntent, GOOGLE_SIGN_IN)
+                }
+            }
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIGN_IN){
+            val task =  GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                loginViewModel.signInWithCredential(GoogleAuthProvider.getCredential(account?.idToken, null))
+            }catch (e: ApiException){
+                Toast.makeText(applicationContext, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
