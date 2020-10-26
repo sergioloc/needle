8715a4f4658db02c96d170cf45cf454d2e2081d4
Sergio López <sergioloc2@gmail.com>
2020-10-26 21:26:21 +0100

    Add default values to user

--- app/src/main/java/com/slc/amarn/models/User.kt
+++ app/src/main/java/com/slc/amarn/models/User.kt
@@ -3,13 +3,13 @@ package com.slc.amarn.models
 import java.io.Serializable
 
 data class User(
-    val name: String,
-    val dateOfBirth: String,
-    val city: String,
-    val gender: Int,
-    val orientation: Int,
-    val description: String,
-    val instagram: String,
-    val facebook: String,
-    val phone: String
+    val name: String = "",
+    val dateOfBirth: String = "",
+    val city: String = "",
+    val gender: Int = 0,
+    val orientation: Int = 0,
+    val description: String = "",
+    val instagram: String = "",
+    val facebook: String = "",
+    val phone: String = ""
 ): Serializable
