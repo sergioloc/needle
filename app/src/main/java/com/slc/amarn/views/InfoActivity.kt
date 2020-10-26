c570b61600c44ba13689510fa81eb5f82ab59f0c
Sergio López <sergioloc2@gmail.com>
2020-10-26 09:29:32 +0100

    New info layout

--- app/src/main/java/com/slc/amarn/views/InfoActivity.kt
+++ app/src/main/java/com/slc/amarn/views/InfoActivity.kt
@@ -2,6 +2,7 @@ package com.slc.amarn.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.slc.amarn.R
 
 class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
