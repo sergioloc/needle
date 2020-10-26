c6ad783fb8f87465c0cb60b334a1432aeaa8c747
Sergio López <sergioloc2@gmail.com>
2020-10-26 21:25:55 +0100

    New Age class

---
+++ app/src/main/java/com/slc/amarn/utils/Age.kt
@@ -0,0 +1,17 @@
+package com.slc.amarn.utils
+
+import java.util.*
+
+class Age {
+    fun getAge(dateOfBirth: String): Int {
+        val date = dateOfBirth.split("-").toTypedArray()
+        val dob = Calendar.getInstance()
+        val today = Calendar.getInstance()
+        dob[date[2].toInt(), date[1].toInt()] = date[0].toInt()
+        var age = today[Calendar.YEAR] - dob[Calendar.YEAR]
+        if (today[Calendar.DAY_OF_YEAR] < dob[Calendar.DAY_OF_YEAR]) {
+            age--
+        }
+        return age
+    }
+}
