5de40dfaa9f732e2034d0085de9da4b516374949
Sergio López <sergioloc2@gmail.com>
2020-10-28 11:35:05 +0100

    Save visibility state in Firestore

--- app/src/main/java/com/slc/amarn/views/SettingsActivity.kt
+++ app/src/main/java/com/slc/amarn/views/SettingsActivity.kt
@@ -4,18 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.slc.amarn.R
+import com.slc.amarn.utils.Info
 import com.slc.amarn.viewmodels.SettingsViewModel
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var settingsViewModel: SettingsViewModel
+    private var initSwitch: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         settingsViewModel = SettingsViewModel()
+        initVariables()
         initButtons()
+    }
+
+    private fun initVariables(){
+        initSwitch = Info.user.visible
+        swt_visible.isChecked = initSwitch
     }
 
     private fun initButtons(){
@@ -27,5 +35,11 @@ class SettingsActivity : AppCompatActivity() {
             settingsViewModel.signOut()
             startActivity(Intent(this, LoginActivity::class.java))
         }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (initSwitch != swt_visible.isChecked)
+            settingsViewModel.modifyVisibility(swt_visible.isChecked)
     }
 }
