c6db9e9456f4b5dbb22cbc3e880c4f9bacb43690
Sergio López <sergioloc2@gmail.com>
2020-10-29 11:16:33 +0100

    New create group dialog

--- app/src/main/java/com/slc/amarn/views/ProfileFragment.kt
+++ app/src/main/java/com/slc/amarn/views/ProfileFragment.kt
@@ -1,17 +1,25 @@
 package com.slc.amarn.views
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.RadioButton
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.slc.amarn.R
 import com.slc.amarn.models.User
@@ -67,6 +75,9 @@ class ProfileFragment : Fragment() {
             intent.putExtra("email", FirebaseAuth.getInstance().currentUser?.email)
             startActivity(intent)
         }
+        cg_create.setOnClickListener {
+            showCreateDialog()
+        }
     }
 
     private fun initObservers(){
@@ -101,5 +112,20 @@ class ProfileFragment : Fragment() {
                 }
             })
         }
+    }
+
+    //Dialogs --------------------------------------------------------------------------------------
+
+    private fun showCreateDialog() {
+        val dialog = Dialog(context!!)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_create_group)
+        dialog.window?.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val btnCreate = dialog.findViewById(R.id.btn_create) as Button
+        btnCreate.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
