76f4481f583b9fed12f1a87e0c1ba67ef114a87a
Sergio López <sergioloc2@gmail.com>
2021-02-09 19:14:52 +0100

    New tab design & indicator

--- app/src/main/java/com/slc/needle/views/WalkthroughActivity.kt
+++ app/src/main/java/com/slc/needle/views/WalkthroughActivity.kt
@@ -6,7 +6,9 @@ import com.slc.needle.R
 import com.slc.needle.adapters.PhotoAdapter
 import com.slc.needle.adapters.WalkthroughAdapter
 import com.slc.needle.models.Step
+import kotlinx.android.synthetic.main.activity_user.*
 import kotlinx.android.synthetic.main.activity_walkthrough.*
+import kotlinx.android.synthetic.main.activity_walkthrough.tab_indicator
 
 class WalkthroughActivity : AppCompatActivity() {
 
@@ -20,5 +22,7 @@ class WalkthroughActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_walkthrough)
         vp_walk.adapter = WalkthroughAdapter(applicationContext, steps)
+        tab_indicator.setupWithViewPager(vp_walk)
+
     }
 }
