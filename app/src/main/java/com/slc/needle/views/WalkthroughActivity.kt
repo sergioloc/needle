68a099b03bc5de0d66e0695c9da618c915d68b2a
Sergio López <sergioloc2@gmail.com>
2021-02-09 18:13:00 +0100

    New empty walkthrough activity

---
+++ app/src/main/java/com/slc/needle/views/WalkthroughActivity.kt
@@ -0,0 +1,12 @@
+package com.slc.needle.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.slc.needle.R
+
+class WalkthroughActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walkthrough)
+    }
+}
